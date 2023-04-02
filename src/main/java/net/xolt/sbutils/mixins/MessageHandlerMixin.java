d31b07c0df5cd8a916055ffcf1a6b387fe02e3c3
hashalite <hashalite@gmail.com>
2023-04-01 20:19:37 -0400

    - '/advert list' now properly displays colors instead of color codes - Mentions no longer notifies you about messages hidden by filters - Made AntiPlace scaffold-compatible - Enchant All will no longer disable if you move an item that has already been enchanted

--- src/main/java/net/xolt/sbutils/mixins/MessageHandlerMixin.java
+++ src/main/java/net/xolt/sbutils/mixins/MessageHandlerMixin.java
@@ -16,10 +16,12 @@ public class MessageHandlerMixin {
     @Inject(method = "onChatMessage", at = @At("HEAD"), cancellable = true)
     private void onChatMessage(SignedMessage message, MessageType.Parameters params, CallbackInfo ci) {
         Text text = params.applyChatDecoration(message.getContent());
-        onMessage(text);
+        preFilterMessage(text);
         if (ChatFilters.shouldFilter(text)) {
             ci.cancel();
+            return;
         }
+        postFilterMessage(text);
     }
 
     @Inject(method = "onGameMessage", at = @At("HEAD"), cancellable = true)
@@ -28,17 +30,22 @@ public class MessageHandlerMixin {
             return;
         }
 
-        onMessage(message);
+        preFilterMessage(message);
         if (ChatFilters.shouldFilter(message)) {
             ci.cancel();
+            return;
         }
+        postFilterMessage(message);
     }
 
-    private static void onMessage(Text message) {
-        Mentions.processMessage(message);
+    private static void preFilterMessage(Text message) {
         ChatLogger.processMessage(message);
         AutoFix.processMessage(message);
         AutoLottery.processMessage(message);
         AutoReply.processMessage(message);
+    }
+
+    private static void postFilterMessage(Text message) {
+        Mentions.processMessage(message);
     }
 }
